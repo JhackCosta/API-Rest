16341308eb12bec184b27273896bb0e3352ffbed
Jhack.Costa <jhack.oliveira@singlepoint.com.br>
2023-05-30 19:03:49 -0300

    Adicionando Spring Security

---
+++ src/main/java/med/voll/api/controller/AutentificacaoController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutentificacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+public class AutentificacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutentificacao dados){
+
+        UsernamePasswordAuthenticationToken token =  new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        Authentication authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+
+    }
+}
